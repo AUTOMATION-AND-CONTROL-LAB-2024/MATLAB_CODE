dfdc5a27e183eda2b995d4d933609e869595e421
Anita00m <anna2104m@gmail.com>
2024-04-05 09:53:04 +0200

    Test robustezza

--- MATLAB_CODE/Controllori/Linear Control 3dof/quadcopterMain3dof.m
+++ MATLAB_CODE/Controllori/Linear Control 3dof/quadcopterMain3dof.m
@@ -79,7 +79,7 @@ K       =   place(Ae,Be,[-0.08 -0.09 -0.04 -0.05 -0.06 -0.07 -10.2 -10.21 -10.22
 
 Kx      =   K(:,1:6);
 Kv      =   K(:,7:9);
-% save("K_pole","Kx","Kv")
+save("K_pole","Kx","Kv")
 % save("K_pole_rob","Kx","Kv")
 open('pole_placement3dof.slx')
 sim('pole_placement3dof.slx',Tend_slk);
--- MATLAB_CODE/Test_controllori/3dof/main_matteo.m
+++ MATLAB_CODE/Test_controllori/3dof/main_matteo.m
@@ -62,13 +62,9 @@ x0          = [1;1;1;0;0;0]*(0.01);    % Initial State Vector - Roll, Pitch, Yaw
 %% Pole Placement control
 load("K_pole.mat");
 
-% % % Robustness (param x2)
-% load("K_pole_rob.mat")
 %% Inverse_dynamic Control
 load("K_inverse.mat");
 
-% % Robustness (param x2)
-% load("K_inverse_rob.mat")
 %% IMU data loading
 IMU_data = load("IMU_data.mat");
 IMU_var_bias_matrix = table2array(IMU_data.IMU_var_bias);      % need IMU_var_bias table
