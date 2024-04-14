9f10210b06d3ed936a7f94c3e9c39db4e01efed7
zagatialex <zagatialex@gmail.com>
2024-04-14 18:51:25 +0200

    Modificato uno script per creare immagine report

--- MATLAB_CODE/LAB/acq_test/Thrust/linear_regr.m
+++ MATLAB_CODE/LAB/acq_test/Thrust/linear_regr.m
@@ -3,11 +3,18 @@ close all;
 clc;
 thrust = [0, 1, 2.7, 3,4.2,5.6]';
 pwm = [1000, 1285, 1548, 1550, 1587,1846]';
+thrust_report = [0, 0.5, 1.5, 2, 2.7, 3, 4.2, 5, 5.6, 6]';
+pwm_report = [1000, 1115, 1285, 1342, 1450, 1550, 1587, 1672, 1846, 1953]';
 delta_x = [2, 2.7]';
 m = [0.49, 0.69]';
 F = 9.81*m;
-plot(delta_x, F);
-%%plot(thrust, pwm);
+%%plot(delta_x, F);
+figure (1);
+plot(thrust_report, pwm_report,'*');
+grid on;
+title("THRUST-PWM COEFFICIENT");
+xlabel("PWM");
+ylabel("THRUST");
 
 
 K_spring = (delta_x'*delta_x)^(-1)*delta_x'*F;
