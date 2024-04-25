f162621230d01a38011ac2e27f164c9efe0353f2
Teomate11 <teo.mastromauro@gmail.com>
2024-04-25 16:08:57 +0200

    EKF2

--- ACQUISITION/RPY_QUATERNION/EKF_script.m
+++ ACQUISITION/RPY_QUATERNION/EKF_script.m
@@ -6,7 +6,7 @@ clc
 g           =       9.81;                   %  gravity acceleration (m/s^2)
 mf          =       [22602; 0; -42062;];    %  earth magnetic field (in inertia frame) (nT) (1nT = 10^-5 Gauss)
 %% extraction of signals from Test_RPY_quaternion
-raw_data = load("RAW_DATA\Test_SENSORS_2.mat");
+raw_data = load("RAW_DATA\Test_SENSORS.mat");
 signals = table2array(raw_data.Acq_Data);   %variable name "Acq_data"
 [rows,colums] = size(signals);
 
@@ -57,7 +57,7 @@ IMU_w_b_variance        = IMU_var_bias_matrix(:,3);         % variance in IMU an
 IMU_mf_b_variance       = ones(3,1)*0.001;                  % variance in IMU magnetic field measurement 
 
 %% RPY computation
-acc_pole                = 30;                               % pole [Hz] of the high-pass-filter of IMU_measurements block
+meas_pole               = 20;                               % pole [Hz] of the high-pass-filter of IMU_measurements block
 RPY_pole                = 50;
 %% EKF parameters
 
@@ -67,29 +67,88 @@ Ts_EKF_integr      = 0.0001;     % [s] Ts for the ODE integration
 Ts_EKF_meas        = 0.01;     % [s] Ts for measurement update
 % --------------------------------------------------------------------------------------------------------
 
-q_phi       = 8e-1;       % q-elements related to phi,theta,yaw states
-q_theta     = 8e-1;
-q_psi       = 50e-0;
+q_ph        = 8e-1;       % q-elements related to phi,theta,yaw states
+q_th        = 8e-1;
+q_ps        = 50e-0;
 q_bias_w_b  = 1e-7;       % q-elements related to bias_wp, bias_wq, bias_wr (bias of wp,wq,wr of angular velocity vector)
-Q           = diag([q_phi,q_theta,q_psi, q_bias_w_b,q_bias_w_b,q_bias_w_b]);
+Q           = diag([q_ph,q_th,q_ps,q_bias_w_b,q_bias_w_b,q_bias_w_b]);
 
-r_ph_meas   = 1e-0;    % variance related to phi computation from IMU measurement
-r_th_meas   = 1e-0;    % variance related to theta computation from IMU measurement
-r_ps_meas   = 15e+1;    % variance related to psi computation from IMU measurement
-R           = diag([r_th_meas,r_th_meas,r_ps_meas]);
+r_ph        = 1e-0;       % variance related to phi computation from IMU measurement
+r_th        = 1e-0;       % variance related to theta computation from IMU measurement
+r_ps        = 15e+1;      % variance related to psi computation from IMU measurement
+R           = diag([r_th,r_th,r_ps]);
 
-ph0         = IMU_roll(1,2);                % initial guess of phi angle
-th0         = IMU_pitch(1,2);                % initial guess of theta angle
-ps0         = IMU_yaw(1,2);                % initial guess of psi angle
+ph0         = IMU_roll(1,2);        % initial guess of phi angle
+th0         = IMU_pitch(1,2);       % initial guess of theta angle
+ps0         = IMU_yaw(1,2);         % initial guess of psi angle
 bias_wp0    = IMU_w_b_bias(1,1);    % initial guess of bias in p direction (of vector w angular velocity) can be estimated with measurements
 bias_wq0    = IMU_w_b_bias(2,1);    % initial guess of bias in q direction (of vector w angular velocity) can be estimated with measurements
 bias_wr0    = IMU_w_b_bias(3,1);    % initial guess of bias in r direction (of vector w angular velocity) can be estimated with measurements
 x0EKF       = [ph0;th0;ps0;bias_wp0;bias_wq0;bias_wr0];
 
-P0_ph_meas  = r_ph_meas;
-P0_th_meas  = r_th_meas;
-P0_ps_meas  = r_ps_meas;
+P0_ph       = 100;
+P0_th       = 100;
+P0_ps       = 100;
 P0_bias_wp  = IMU_w_b_variance(1,1);
 P0_bias_wq  = IMU_w_b_variance(2,1);
 P0_bias_wr  = IMU_w_b_variance(3,1);
-P0          = diag([P0_ph_meas,P0_th_meas,P0_ps_meas,P0_bias_wp,P0_bias_wq,P0_bias_wr]);
+P0          = diag([P0_ph,P0_th,P0_ps,P0_bias_wp,P0_bias_wq,P0_bias_wr]);
+
+%% EKF2 parameters
+
+% % --------------------------------------------------------------------------------------------------------
+% % oss: Ts_EKF_meas must be a multiple of Ts_EKF_integr, example: Ts_EKF_meas = 10 * Ts_EKF_integr
+% Ts_EKF_integr      = 0.0001;     % [s] Ts for the ODE integration 
+% Ts_EKF_meas        = 0.01;     % [s] Ts for measurement update
+% % --------------------------------------------------------------------------------------------------------
+% 
+% % RP_EKF
+% q_ph        = 630e-0;                 % q-elements related to phi states
+% q_th        = 1300e-0;                 % q-elements related to theta states
+% q_bias_w_b  = 1e-7;                 % q-elements related to bias_wp, bias_wq, bias_wr (bias of wp,wq,wr of angular velocity vector)
+% Q           = diag([q_ph,q_th,q_bias_w_b,q_bias_w_b,q_bias_w_b]);
+% 
+% r_ph        = 400e-0;                 % variance related to phi computation from IMU measurement
+% r_th        = 900e-0;                 % variance related to theta computation from IMU measurement
+% R           = diag([r_ph,r_th]);
+% 
+% ph0         = IMU_roll(1,2);        % initial guess of phi angle
+% th0         = IMU_pitch(1,2);       % initial guess of theta angle
+% bias_wp0    = IMU_w_b_bias(1,1);    % initial guess of bias in p direction (of vector w angular velocity) can be estimated with measurements
+% bias_wq0    = IMU_w_b_bias(2,1);    % initial guess of bias in q direction (of vector w angular velocity) can be estimated with measurements
+% bias_wr0    = IMU_w_b_bias(3,1);    % initial guess of bias in r direction (of vector w angular velocity) can be estimated with measurements
+% x0EKF       = [ph0;th0;bias_wp0;bias_wq0;bias_wr0];
+% 
+% P0_ph       = 100;
+% P0_th       = 100;
+% P0_bias_wp  = IMU_w_b_variance(1,1);
+% P0_bias_wq  = IMU_w_b_variance(2,1);
+% P0_bias_wr  = IMU_w_b_variance(3,1);
+% P0          = diag([P0_ph,P0_th,P0_bias_wp,P0_bias_wq,P0_bias_wr]);
+% 
+% % Y_EKF
+% q_ps        = 2000e-0;                % q-elements related to yaw states
+% r_ps        = 1500e+0;                % variance related to psi computation from IMU measurement
+% ps0         = IMU_yaw(1,2);         % initial guess of psi angle
+% P0_ps       = 100;
+% 
+% % RPY_rate EKF
+% q_ph_rate       = 900;       % q-elements related to phi,theta,yaw states
+% q_th_rate       = 900;
+% q_ps_rate       = 900;
+% Q_rate          = diag([q_ph_rate,q_th_rate,q_ps_rate]);
+% 
+% r_ph_rate        = 40;       % variance related to phi computation from IMU measurement
+% r_th_rate        = 40;       % variance related to theta computation from IMU measurement
+% r_ps_rate        = 40;      % variance related to psi computation from IMU measurement
+% R_rate           = diag([r_th_rate,r_th_rate,r_ps_rate]);
+% 
+% ph_rate0         = IMU_roll(1,2);        % initial guess of phi angle
+% th_rate0         = IMU_pitch(1,2);       % initial guess of theta angle
+% ps_rate0         = IMU_yaw(1,2);         % initial guess of psi angle
+% x0_rate_EKF      = [ph_rate0;th_rate0;ps_rate0;];
+% 
+% P0_ph_rate       = 10;
+% P0_th_rate       = 10;
+% P0_ps_rate       = 10;
+% P0_rate          = diag([P0_ph_rate,P0_th_rate,P0_ps_rate]);
--- MATLAB_CODE/Matteo/main_matteo.m
+++ MATLAB_CODE/Matteo/main_matteo.m
@@ -62,19 +62,23 @@ poles = -[0.1,0.12,0.13,0.14,0.15,0.16];
 %% PolePlacement Control
 load("K_pole_inv.mat");
 
-%% IMU data loading
+%% IMU parameter loading
 IMU_data = load("IMU_data.mat");
 IMU_var_bias_matrix = table2array(IMU_data.IMU_var_bias);      % need IMU_var_bias table
 clear IMU_data;
-%% <IMU_measurements> parameters
-IMU_a_b_bias          = ones(3,1)*0.001;                    % bias in IMU linear acceleration measurement     (THEORY)
-IMU_w_b_bias          = IMU_var_bias_matrix(:,2);         % bias in IMU angular velocity measurement 
-IMU_mf_b_bias         = ones(3,1)*0.001;                    % bias in IMU magnetic field measurement          (THEORY)
-IMU_a_b_variance      = IMU_var_bias_matrix(:,1);         % variance in IMU linear acceleration measurement
-IMU_w_b_variance      = IMU_var_bias_matrix(:,3);         % variance in IMU angular velocity measurement
-IMU_mf_b_variance     = ones(3,1)*0.001;         % variance in IMU magnetic field measurement 
 
-IMU_pole              = 10;                               % pole of the high-pass-filter of IMU_measurements block
+%% IMU parameters assignment
+IMU_a_b_bias            = ones(3,1)*0.001;                  % bias in IMU linear acceleration measurement     (THEORY)
+IMU_w_b_bias            = IMU_var_bias_matrix(:,2);         % bias in IMU angular velocity measurement 
+IMU_mf_b_bias           = ones(3,1)*0.001;                  % bias in IMU magnetic field measurement          (THEORY)
+IMU_a_b_variance        = IMU_var_bias_matrix(:,1);         % variance in IMU linear acceleration measurement
+IMU_w_b_variance        = IMU_var_bias_matrix(:,3);         % variance in IMU angular velocity measurement
+IMU_mf_b_variance       = ones(3,1)*0.001;                  % variance in IMU magnetic field measurement 
+
+IMU_pole                = 10;
+%% RPY computation
+meas_pole               = 20;                               % pole [Hz] of the high-pass-filter of IMU_measurements block
+RPY_pole                = 50;
 %% rotation from IMU-frame to BODY-frame
 angleZ = pi/4;
 Rbody_frd = [cos(angleZ) -sin(angleZ)   0;
@@ -88,35 +92,94 @@ Rbody_ned = Rbody_frd*Rfrd_ned;
 clear angleZ angleX Rfrd_ned;
 %% EKF parameters
 
+% % --------------------------------------------------------------------------------------------------------
+% % oss: Ts_EKF_meas must be a multiple of Ts_EKF_integr, example: Ts_EKF_meas = 10 * Ts_EKF_integr
+% Ts_EKF_integr      = 0.0001;     % [s] Ts for the ODE integration 
+% Ts_EKF_meas        = 0.01;     % [s] Ts for measurement update
+% % --------------------------------------------------------------------------------------------------------
+% 
+% q_ph        = 8e-1;       % q-elements related to phi,theta,yaw states
+% q_th        = 8e-1;
+% q_ps        = 50e-0;
+% q_bias_w_b  = 1e-7;       % q-elements related to bias_wp, bias_wq, bias_wr (bias of wp,wq,wr of angular velocity vector)
+% Q           = diag([q_ph,q_th,q_ps,q_bias_w_b,q_bias_w_b,q_bias_w_b]);
+% 
+% r_ph        = 1e-0;       % variance related to phi computation from IMU measurement
+% r_th        = 1e-0;       % variance related to theta computation from IMU measurement
+% r_ps        = 15e+1;      % variance related to psi computation from IMU measurement
+% R           = diag([r_th,r_th,r_ps]);
+% 
+% ph0         = IMU_roll(1,2);        % initial guess of phi angle
+% th0         = IMU_pitch(1,2);       % initial guess of theta angle
+% ps0         = IMU_yaw(1,2);         % initial guess of psi angle
+% bias_wp0    = IMU_w_b_bias(1,1);    % initial guess of bias in p direction (of vector w angular velocity) can be estimated with measurements
+% bias_wq0    = IMU_w_b_bias(2,1);    % initial guess of bias in q direction (of vector w angular velocity) can be estimated with measurements
+% bias_wr0    = IMU_w_b_bias(3,1);    % initial guess of bias in r direction (of vector w angular velocity) can be estimated with measurements
+% x0EKF       = [ph0;th0;ps0;bias_wp0;bias_wq0;bias_wr0];
+% 
+% P0_ph       = 100;
+% P0_th       = 100;
+% P0_ps       = 100;
+% P0_bias_wp  = IMU_w_b_variance(1,1);
+% P0_bias_wq  = IMU_w_b_variance(2,1);
+% P0_bias_wr  = IMU_w_b_variance(3,1);
+% P0          = diag([P0_ph,P0_th,P0_ps,P0_bias_wp,P0_bias_wq,P0_bias_wr]);
+
+%% EKF2 parameters
+
 % --------------------------------------------------------------------------------------------------------
 % oss: Ts_EKF_meas must be a multiple of Ts_EKF_integr, example: Ts_EKF_meas = 10 * Ts_EKF_integr
 Ts_EKF_integr      = 0.0001;     % [s] Ts for the ODE integration 
 Ts_EKF_meas        = 0.01;     % [s] Ts for measurement update
 % --------------------------------------------------------------------------------------------------------
 
-q_phi     = 3e-2;       % q-elements related to phi,theta,yaw states
-q_theta   = 1e-2;
-q_psi     = 8e-3;
-q_bias_w_b  = 1e-7;       % q-elements related to bias_wp, bias_wq, bias_wr (bias of wp,wq,wr of angular velocity vector)
-Q           = diag([q_phi,q_theta,q_psi, q_bias_w_b,q_bias_w_b,q_bias_w_b]);
+% RP_EKF
+q_ph        = 630e-0;                 % q-elements related to phi states
+q_th        = 1300e-0;                 % q-elements related to theta states
+q_bias_w_b  = 1e-7;                 % q-elements related to bias_wp, bias_wq, bias_wr (bias of wp,wq,wr of angular velocity vector)
+Q           = diag([q_ph,q_th,q_bias_w_b,q_bias_w_b,q_bias_w_b]);
 
-r_ph_meas   = 1e-3;    % variance related to phi computation from IMU measurement
-r_th_meas   = 8e-3;    % variance related to theta computation from IMU measurement
-r_ps_meas   = 1e-2;    % variance related to psi computation from IMU measurement
-R           = diag([r_th_meas,r_th_meas,r_ps_meas]);
+r_ph        = 400e-0;                 % variance related to phi computation from IMU measurement
+r_th        = 900e-0;                 % variance related to theta computation from IMU measurement
+R           = diag([r_ph,r_th]);
 
-ph0         = -pi/4;                % initial guess of phi angle
-th0         = -pi/4;                % initial guess of theta angle
-ps0         = -pi/4;                % initial guess of psi angle
+ph0         = 0;                    % initial guess of phi angle
+th0         = 0;                    % initial guess of theta angle
 bias_wp0    = IMU_w_b_bias(1,1);    % initial guess of bias in p direction (of vector w angular velocity) can be estimated with measurements
 bias_wq0    = IMU_w_b_bias(2,1);    % initial guess of bias in q direction (of vector w angular velocity) can be estimated with measurements
 bias_wr0    = IMU_w_b_bias(3,1);    % initial guess of bias in r direction (of vector w angular velocity) can be estimated with measurements
-x0EKF       = [ph0;th0;ps0;bias_wp0;bias_wq0;bias_wr0];
+x0EKF       = [ph0;th0;bias_wp0;bias_wq0;bias_wr0];
 
-P0_ph_meas  = r_ph_meas;
-P0_th_meas  = r_th_meas;
-P0_ps_meas  = r_ps_meas;
+P0_ph       = 100;
+P0_th       = 100;
 P0_bias_wp  = IMU_w_b_variance(1,1);
 P0_bias_wq  = IMU_w_b_variance(2,1);
 P0_bias_wr  = IMU_w_b_variance(3,1);
-P0          = diag([P0_ph_meas,P0_th_meas,P0_ps_meas,P0_bias_wp,P0_bias_wq,P0_bias_wr]);
+P0          = diag([P0_ph,P0_th,P0_bias_wp,P0_bias_wq,P0_bias_wr]);
+
+% Y_EKF
+q_ps        = 2000e-0;                  % q-elements related to yaw states
+r_ps        = 1500e+0;                  % variance related to psi computation from IMU measurement
+ps0         = 0;                        % initial guess of psi angle
+P0_ps       = 100;
+
+% RPY_rate EKF
+q_ph_rate       = 900;       % q-elements related to phi,theta,yaw states
+q_th_rate       = 900;
+q_ps_rate       = 900;
+Q_rate          = diag([q_ph_rate,q_th_rate,q_ps_rate]);
+
+r_ph_rate        = 40;       % variance related to phi computation from IMU measurement
+r_th_rate        = 40;       % variance related to theta computation from IMU measurement
+r_ps_rate        = 40;      % variance related to psi computation from IMU measurement
+R_rate           = diag([r_th_rate,r_th_rate,r_ps_rate]);
+
+ph_rate0         = 0;        % initial guess of phi angle
+th_rate0         = 0;       % initial guess of theta angle
+ps_rate0         = 0;         % initial guess of psi angle
+x0_rate_EKF      = [ph_rate0;th_rate0;ps_rate0;];
+
+P0_ph_rate       = 10;
+P0_th_rate       = 10;
+P0_ps_rate       = 10;
+P0_rate          = diag([P0_ph_rate,P0_th_rate,P0_ps_rate]);
