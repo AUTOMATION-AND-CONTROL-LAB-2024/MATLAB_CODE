083480ce47724ddeb9f45cfa8e73fdb66f0e332a
Anita00m <anna2104m@gmail.com>
2024-05-10 16:57:48 +0200

    WIP Robustness
    + file con matrici ss representation dello stable sys di H2 1dof

--- MATLAB_CODE/Anna/Robustness/h2/1dof/EKF_main_H2_1dof.m
+++ MATLAB_CODE/Anna/Robustness/h2/1dof/EKF_main_H2_1dof.m
@@ -3,17 +3,20 @@ clear all
 close all
 clc
 
-%% Model parameters 
+%% Model parameters  
 g           =       9.81;                   %  gravity acceleration (m/s^2)
 mf          =       [22602; 0; -42062;];    %  earth magnetic field (in inertia frame) (nT) (1nT = 10^-5 Gauss)
 Ixx         =       0.015;                  %  moment of inertia (kg*m^2)
 Iyy         =       0.015;                  %  moment of inertia (kg*m^2)
 Izz         =       0.03;                   %  moment of inertia (kg*m^2)
 l           =       0.25;                   %  quadcopter arm (m)
-k           =       0.0022;                 %  lift constant
+k1          =       0.00684324;             %  lift constant motor 1
+k2          =       0.00703859;             %  lift constant motor 2
+k3          =       0.00814964;             %  lift constant motor 3
+k4          =       0.00746697;             %  lift constant motor 4
 b           =       1.14e-7;                %  drag constant
 m           =       0.61+0.064*4;           %  mass (kg)
-param       =       [Ixx;Iyy;Izz;l;k;b;m];
+param       =       [Ixx;Iyy;Izz;l;k1;k2;k3;k4;b;m];
  
 % cph         =       cos(phi);
 % cth         =       cos(theta);
