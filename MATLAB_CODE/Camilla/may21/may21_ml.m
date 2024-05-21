200e7caaf62f1734b538a3f40c5479e2d6beaaf2
Anita00m <anna2104m@gmail.com>
2024-05-21 14:15:13 +0200

    ops per camilla

--- MATLAB_CODE/Camilla/may21/may21_ml.m
+++ MATLAB_CODE/Camilla/may21/may21_ml.m
@@ -15,19 +15,17 @@ Ixx = 0.055;
 
 G = 1/Ixx/s^2;
 
-%% Study stuff
-
-figure(1)
-bode(G)
-grid on
-
 %% Controller
 
 Kc = 0.04/Ixx;
 tau_i = 10.74;
 tau_d = 10.79;
 
-PID = Kc * (tau_i*s+1) * (tau_d*s+1) / (tau_i*s) / (s+10);
+PID = Kc * (tau_i*s+1) * (tau_d*s+1) / (tau_i*s) / (s+30);
 
 PID_D = c2d(PID, Ts_slk);
+
+figure(1)
+bode(G*PID)
+grid on
 
