e8d7691bd99d6bb6c11417a10706182495ada035
zagatialex <zagatialex@gmail.com>
2024-05-30 22:40:11 +0200

    Updated step fitting, no major changes

--- ACQUISITION/SYSTEM_RESPONSE_1DOF_PID/step_fitting.py
+++ ACQUISITION/SYSTEM_RESPONSE_1DOF_PID/step_fitting.py
@@ -9,6 +9,7 @@ import scipy.optimize
 from tbcontrol.responses import fopdt, sopdt
 from ipywidgets import interact
 import numpy as np
+from tbcontrol.numeric import skogestad_half
 
 
 filename = "step_acq.xlsx"
@@ -17,13 +18,24 @@ df = pd.read_excel(filename)
 time = np.arange(0, 19, 0.01)
 Y_ref = df["ROLL_REF"][4700:6600]
 Y     = df["ROLL"][4700:6600]+3.9*3.14/180
-[K, tau, theta, y0], _ = scipy.optimize.curve_fit(fopdt, time, Y, [2, 4, 1, 10])
-[K_2, tau_2, zeta_2, theta_2, y0_2], _ = scipy.optimize.curve_fit(sopdt, time, Y, [2, 2, 1.5, 1, 10])
-plt.plot(time, Y)
-plt.plot(time, Y_ref)
-#plt.plot(time, fopdt(time, K, tau, theta, y0), color='red', label='FOPDT fit')
-plt.plot(time, sopdt(time, K_2, tau_2, zeta_2, theta_2, y0_2), color='red', label='SOPDT fit')
-plt.grid()
-plt.show()
 
+def plotstep(G, D, T=None):
+    t, y = control.step_response(G, T=15)
+    new_t = numpy.concatenate([[0], t + D])
+    new_y = numpy.concatenate([[0], y])
+    plt.plot(new_t, new_y)
 
+def step_fitting(Y):
+    [K, tau, theta, y0], _ = scipy.optimize.curve_fit(fopdt, time, Y, [2, 4, 1, 10])
+    [K_2, tau_2, zeta_2, theta_2, y0_2], _ = scipy.optimize.curve_fit(sopdt, time, Y, [2, 2, 1.5, 1, 10])
+    plt.plot(time, Y)
+    plt.plot(time, Y_ref)
+    plt.plot(time, sopdt(time, K_2, tau_2, zeta_2, theta_2, y0_2), color='red', label='SOPDT fit')
+    print([K_2, tau_2, zeta_2, theta_2, y0_2])
+    plt.grid()
+    plt.show()
+    return()
+
+# LIBRARY TEST
+step_fitting(Y)
+print()
