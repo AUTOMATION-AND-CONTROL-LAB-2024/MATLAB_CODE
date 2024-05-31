1e52b06ae7a7d4d096b83bb1af5701f55b050f57
Teomate11 <teo.mastromauro@gmail.com>
2024-05-31 09:39:25 +0200

    noise analysis

---
+++ MATLAB_CODE/IMU_parameter_identification/magnitudelFFT.m
@@ -0,0 +1,16 @@
+function [magnitude,freq] = magnitudelFFT(signal,Ts)
+% signal    = input signal
+% Ts        = sampling time of input signal [s]
+
+Fs   = 1/Ts;                                    % Sampling frequency   [Hz]    
+L    = length(signal);                          % Length of signal
+if mod(L,2) ~= 0
+    L = L-1;                                    % make the length of the signal even
+end
+spectrum = fft(signal);
+magnitude = abs(spectrum/L);
+magnitude = magnitude(1:L/2+1);
+magnitude(2:end-1) = 2*magnitude(2:end-1);
+
+freq = Fs/L*(0:L/2);
+end
